2ff768b7251676ff67b4fc7a01080c11076540ef
Benen Huntley <benen.huntley@gmail.com>
2024-01-12 12:14:26 +1030

    loading spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+import { LoadingPage } from "~/components/loadingSpinner";
 dayjs.extend(relativeTime);
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
@@ -39,7 +42,13 @@ const PostView = (props: PostWithUser) => {
   console.log(createdAt);
   return (
     <div className="flex gap-3 border-b border-slate-400 p-8" key={id}>
-      <img src={author?.imageUrl} className="me-2 h-10 w-10 rounded-full" />
+      <Image
+        src={author!.imageUrl}
+        alt={`@${author!.username}'s profile image`}
+        className="me-2 h-10 w-10 rounded-full"
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex text-slate-400">
           <span className="text-slate-300">@{author?.username}</span>
@@ -60,10 +69,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-4 border-b p-4">
-      <img
+      <Image
         src={user.imageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
+        width={1}
+        height={1}
       />
       <input
         placeholder="Type some stuff"
@@ -79,7 +90,7 @@ export default function Home() {
   const { data, isLoading } = api.posts.getAll.useQuery();
   // const { quote } = api.yahooFinance.quote("SYI:AX").run();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <LoadingPage />;
   if (!data) return <div>Something went wrong</div>;
 
   return (
@@ -92,8 +103,14 @@ export default function Home() {
       <main className="flex h-screen justify-center">
         <div className="w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex justify-between border-b border-slate-400 bg-slate-800 p-4">
-            <UserButton afterSignOutUrl="/" />
-            {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+            <div className="flex items-center">
+              {/* Left Side Content */}
+              {/* <UserButton afterSignOutUrl="/" /> */}
+            </div>
+            <div className="flex items-center">
+              {/* Right Side Content */}
+              {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+            </div>
           </div>
           {user.isSignedIn && <CreatePostWizard />}
           <div className="flex flex-col">
